9792db6102ebc0d7df34885644157cf6342853b0
RSilva9 <ramasilva909@gmail.com>
2022-11-07 22:20:41 -0300

    Cambios para preentrega 2
    Agregado objetos, funciones, etc

--- js/main.js
+++ js/main.js
@@ -1,120 +1,100 @@
-alert("Bienvenido a la tienda online")
+function Caja(tam, ad, pr){
+     this.tamano = tam;
+     this.adicional = ad;
+     this.precio = pr;
+}
 
-let listaCarrito = "Carrito: "
+const cajaInd = new Caja("individual", "nada", 2800);
+const cajaDob = new Caja("doble", "nada", 5600);
+const cajaTri = new Caja("triple", "nada", 8000);
+const cajaIndChoc = new Caja("individual", "chocolate", 3000);
+const cajaIndCop = new Caja("individual", "copa", 4500);
+
+const carrito = []
+const productos = [cajaInd, cajaDob, cajaTri, cajaIndChoc, cajaIndCop]
+
 let precioFinal = 0
+
+alert("Bienvenido a la tienda online")
 menu()
 
-function salir(){
-    alert("Fin de la compra.")
-}
-
-function catalogo(){
-    console.log("________________________")
-    console.log("1 - Caja de vino individual - $2800.")
-    console.log("2 - Caja de vino doble - $5600.")
-    console.log("3 - Caja de vino triple - $8000.")
-    console.log("4 - Caja de vino individual con chocolate - $3000.")
-    console.log("5 - Caja de vino individual con copa - $4500.")
-    console.log("6 - Atrás.")
-    
-    let opt = prompt("Elegir opción.");
-    do{
-        switch(opt){
-            case "1":
-                if(listaCarrito == "Carrito: "){
-                    listaCarrito = listaCarrito + "Caja de vino individual - $2800"
-                }else{
-                    listaCarrito = listaCarrito + ", Caja de vino individual - $2800"
-                }
-                
-                precioFinal += 2800
-                alert("Caja de vino individual agregado al carrito")
-                break
-            case "2":
-                if(listaCarrito == "Carrito: "){
-                    listaCarrito = listaCarrito + "Caja de vino doble - $5600"
-                }else{
-                    listaCarrito = listaCarrito + ", Caja de vino doble - $5600"
-                }
-                alert("Caja de vino doble agregado al carrito")
-                precioFinal += 5600
-                break
-            case "3":
-                if(listaCarrito == "Carrito: "){
-                    listaCarrito = listaCarrito + "Caja de vino triple - $8000"
-                }else{
-                    listaCarrito = listaCarrito + ", Caja de vino triple - $8000"
-                }
-                alert("Caja de vino triple agregado al carrito")
-                precioFinal += 8000
-                break
-            case "4":
-                if(listaCarrito == "Carrito: "){
-                    listaCarrito = listaCarrito + "Caja de vino individual con chocolate - $3000"
-                }else{
-                    listaCarrito = listaCarrito + ", Caja de vino individual con chocolate - $3000"
-                }
-                alert("Caja de vino individual con chocolate agregado al carrito")
-                precioFinal += 3000
-                break
-            case "5":
-                if(listaCarrito == "Carrito: "){
-                    listaCarrito = listaCarrito + "Caja de vino individual con copa. - $4500"
-                }else{
-                    listaCarrito = listaCarrito + ", Caja de vino individual con copa. - $4500"
-                }
-                alert("Caja de vino individual con copa agregado al carrito")
-                precioFinal += 4500
-                break
-            case "6":
-                menu()
-                break
-        }
-        opt = prompt("Elegir opción.");
-    }while(opt != "6")
-
-    if(opt="6"){
-        menu()
-    }
-}
-
-function carrito(){
-    console.log(listaCarrito)
-    console.log("Precio final: $" + precioFinal)
-
-    pm = prompt("1- Pagar. 2- Volver al menu.")
-    if(pm == "1"){
-        if(precioFinal == 0){
-            alert("El carrito está vacío.")
-            menu()
-        }else{
-            alert("Pago realizado.")
-            salir()
-        }
-        
-    }else if (pm == "2"){
-        menu()
-    }
-    
-}
-
 function menu(){
-    console.log("________________________")
-    console.log("1 - Ver catálogo.")
-    console.log("2 - Ver carrito.")
-    console.log("3 - Salir.")
-
-    let opti = prompt("Elegir opción.")
-    switch(opti){
+    let opt1 = prompt("Elegir opción: \n1 - Ver catálogo. \n2 - Ver carrito. \n3 - Salir.");
+    switch(opt1){
         case "1":
             catalogo()
             break
         case "2":
-            carrito()
+            carro()
             break
         case "3":
-            salir()
+            (function salir(){alert("Fin de la compra.")})()
             break
+        default:
+            alert("Opción inexistente.")
+            menu()
     }
 }
 
+function catalogo(){
+    console.log("____________________________")
+    let i=1;
+    for(const item of productos){ //Muestra todas
+        if(item.adicional == "nada"){
+             console.log(i + " - Caja de vino " + item.tamano + " - $" + item.precio)
+        }else{
+             console.log(i+ " - Caja de vino " + item.tamano + " con " + item.adicional + " - $" + item.precio)
+        }
+        i+=1
+    }
+    console.log(i + " - Atrás.")
+    
+    let optcat = prompt("Elegir opción:")
+    while(optcat != i){
+        if(optcat > productos.length+1){
+            alert("Opción inexistente")
+            catalogo()
+        }else{
+            precioFinal += productos[optcat-1].precio
+            if(productos[optcat-1].adicional == "nada"){
+                alert("Caja de vino " + productos[optcat-1].tamano + " agregado al carrito.")
+            }else{
+                alert("Caja de vino " + productos[optcat-1].tamano + " con " + productos[optcat-1].adicional + " agregado al carrito.")
+            }
+            carrito.push(productos[optcat-1])
+        }
+        
+        optcat = prompt("Elegir opción:")
+    }
+    menu()
+}
+
+function carro(){
+    if(carrito.length == 0){
+        alert("El carrito está vacío")
+        menu()
+    }else{
+        let i=1
+        console.log("-------- Carrito --------")
+        for(const item of carrito){ //Muestra todas
+            if(item.adicional == "nada"){
+                console.log(i + " - Caja de vino " + item.tamano + " - $" + item.precio)
+            }else{
+                console.log(i+ " - Caja de vino " + item.tamano + " con " + item.adicional + " - $" + item.precio)
+            }
+            i+=1
+        }
+        console.log("Precio final a pagar: $" + precioFinal)
+        let pm = prompt("1- Pagar. \n2- Volver al menu.")
+        if(pm == "1"){
+            alert("Pago realizado.")
+        }else if (pm == "2"){
+            menu()
+        }else if (pm != "1" || "2"){
+            alert("Opción inexistente.")
+            carro()
+        }
+    }
+}
+
+    
