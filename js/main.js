a555264672c835a15998f61ce9bb379be07d2806
RSilva9 <ramasilva909@gmail.com>
2022-11-23 21:32:55 -0300

    Storage aplicado

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-const carrito = []
-let precioFinal = 0
+const carrito = JSON.parse(localStorage.getItem('carrito')) || []
+let precioFinal = JSON.parse(localStorage.getItem('precioFinal')) || 0
+var inputs = JSON.parse(localStorage.getItem('inputs')) || document.querySelectorAll('input[type="checkbox"]')
+for(let i of inputs){
+    document.getElementById(i.id).checked = i.checked
+}
+
+actCarrito()
 
 document.addEventListener('DOMContentLoaded', ()=>{
     const productos = document.querySelectorAll('.inputBox input')
-   
+
     for(let prod of productos){
+
         prod.addEventListener('change', (evt)=>{
             if(evt.currentTarget.checked){
                 let title = evt.currentTarget.parentElement.parentElement.dataset.title
@@ -18,30 +25,34 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 
                 let index = carrito.indexOf(title + " - $" + price);
                 if (index >= 0) {
-                    carrito.splice( index, 1 );
+                carrito.splice( index, 1 );
                 }
 
                 precioFinal -= Number(price)
-                
             }
             actCarrito()
             
+
+            inputs = document.querySelectorAll('input[type="checkbox"]')
+            var arrData = []
+            for(let c of inputs){
+                arrData.push({ id: c.id, checked: c.checked })
+            }
+            localStorage.setItem('inputs', JSON.stringify(arrData))
         })
-        
     }
 })
 
 function actCarrito(){
+    localStorage.setItem('precioFinal', JSON.stringify(precioFinal))
+    localStorage.setItem('carrito', JSON.stringify(carrito))
     let html=""
     for(let prod of carrito){
         if(title =! null){
             html += `
-            <li>${prod}</li>
-            `
-        }else{
-            
-        }
-        
+             <li>${prod}</li>
+             `
+    }
     }
 
     let ul = document.querySelector("#carrito ul")
@@ -64,12 +75,7 @@ btn.onclick = (evt)=>{
         let carroPago = document.querySelector("#carrito")
         carroPago.innerHTML = pago
 
-        const btnR = document.querySelector('#btnReset')
-        btnR.onclick = (evt) => {
-        evt.preventDefault()
-        window.location.reload()
-        }
-    }else{
+        }else{
         evt.preventDefault()
         let pago = 
         `
@@ -80,21 +86,14 @@ btn.onclick = (evt)=>{
         `
         let carroPago = document.querySelector("#carrito")
         carroPago.innerHTML = pago
-
+        
+        }
         const btnR = document.querySelector('#btnReset')
         btnR.onclick = (evt) => {
         evt.preventDefault()
         window.location.reload()
-        }
-    }   
+        localStorage.removeItem('carrito')
+        localStorage.removeItem('precioFinal')
+        localStorage.removeItem('inputs')
+        }   
 }
-
-
-    
-    
-    
-    
-    
-    
-    
-    
